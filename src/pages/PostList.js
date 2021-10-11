7a56ade63cb4e57edd0d4a0ce32895064921078a
kimhanjune93 <kimhanjune93@gmail.com>
2021-10-12 00:07:46 +0900

    (김한준) Route기능

---
+++ src/pages/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from "../elements/Grid";
+const PostList = (props) => {
+    return (
+        <React.Fragment>
+            <Grid>
+                ㅁㄴㅇㅁㄴㅇ
+            </Grid>
+        </React.Fragment>
+    );
+};
+
+export default PostList;
