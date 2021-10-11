d3b44dadd3f2c9a272bf1691cc44dd18d9dcbb33
geemhunsu <sksguss2@gmail.com>
2021-10-11 23:34:03 +0900

    elements Grid, Image add

---
+++ src/elements/Grid.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Grid = (props) => {
+    const {
+        children, 
+        margin, 
+        padding, 
+        width, 
+        height, 
+        bg, 
+        flex, // justify-content의 값 설정
+        is_center, // 글자 가운데 정렬
+        _onClick
+    } = props;
+    const styles = { // props 중에서 스타일 부분만 따로 묶음
+        margin, 
+        padding,
+        width, 
+        height, 
+        bg, 
+        flex, 
+        is_center}
+    
+    return (
+        <React.Fragment>
+            <GridBox {...styles} onClick={_onClick}>{children}</GridBox>
+        </React.Fragment>
+    );
+};
+
+Grid.defaultProps = {
+    children: null,
+    margin: null,
+    padding: null,
+    width: null,
+    height: "100%",
+    bg: null,    
+    flex: null,
+    flex_direction: null,
+    is_center: false,
+    _onClick: () => {},
+
+}
+
+// flex의 경우 justify-content의 값만 바꾸는 것으로
+const GridBox = styled.div`
+    margin: ${props => props.margin};
+    padding: ${props => props.padding};
+    width: ${props => props.width};
+    height: ${props => props.height};
+    background-color: ${props => props.bg};    
+    ${props => props.flex ? 
+        `display: flex; align-items: center; justify-content: ${props.flex};` : ""}
+    ${props => props.is_center ?
+     `text-align: center;` : ""}
+    flex-direction: ${props => props.flex_direction};
+`;
+
+export default Grid;
---
+++ src/elements/index.js
@@ -0,0 +1,5 @@
+import Grid from "./Grid";
+import Image from "./Image";
+import Button from "./Button";
+
+export {Grid, Image, Button};
