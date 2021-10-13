ff9a22b9a5581bcc0752fd5804d4622d1f071062
happyKale <syk767@gmail.com>
2021-10-13 20:30:15 +0900

    (김세연) input 컴포넌트 props 추가

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -1,70 +1,95 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Input = (props) => {
+  const {
+    _onChange,
+    placeholder,
+    multiple,
+    width,
+    height,
+    size,
+    color,
+    margin,
+    padding,
+    border,
+    radius,
+    family,
+    type,
+    outline,
+    outlinecolor,
+    onSubmit,
+    value,
+  } = props;
 
-    const { _onChange, placeholder, multiple, width, height, size,
-        color, margin, padding, border, radius, family, type, outline, 
-        outlinecolor, onSubmit, value, } = props;
-    
-    const styles = {
-        width: width,
-        height: height,
-        size: size,
-        color: color,
-        margin: margin,
-        padding: padding,
-        border: border,
-        radius: radius,
-        family: family,
-        type: type,
-        outline: outline,
-        outlinecolor:outlinecolor,
-        value:value,
-    }
+  const styles = {
+    width: width,
+    height: height,
+    size: size,
+    color: color,
+    margin: margin,
+    padding: padding,
+    border: border,
+    radius: radius,
+    family: family,
+    type: type,
+    outline: outline,
+    outlinecolor: outlinecolor,
+  };
 
-    if(multiple){
-        return(
-            <React.Fragment>
-                <InputTextArea {...styles} onKeyPress={(e) => {
-                    onSubmit(e);
-                }} onChange={_onChange} placeholder={placeholder}/>
-            </React.Fragment>
-        );
-    }
-
-    return(
-        <React.Fragment>
-            <InputText {...styles} type={type} onKeyPress={(e) => {
-                if(e.key === "Enter"){
-                    onSubmit(e);
-                }
-            }} onChange={_onChange} placeholder={placeholder}/>
-        </React.Fragment>
+  if (multiple) {
+    return (
+      <React.Fragment>
+        <InputTextArea
+          {...styles}
+          onKeyPress={(e) => {
+            onSubmit(e);
+          }}
+          onChange={_onChange}
+          placeholder={placeholder}
+        />
+      </React.Fragment>
     );
-}
+  }
+
+  return (
+    <React.Fragment>
+      <InputText
+        {...styles}
+        type={type}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            onSubmit(e);
+          }
+        }}
+        onChange={_onChange}
+        placeholder={placeholder}
+      />
+    </React.Fragment>
+  );
+};
 
 Input.defaultProps = {
-    _onChange: () => {},
-    width: "100%",
-    height: null,
-    size: null, //폰트 크기
-    margin: "0px",
-    padding: "0px",
-    color: null, //폰트 색상
-    border: null,
-    radius: null, //border-radius
-    family: null, //폰트 타입
-    type: "text",
-    outline: false, // 아웃라인 유무
-    outlinecolor: null, // 아웃라인 색상
-    onSubmit: () => {},
-    value: null,
-}
+  _onChange: () => {},
+  width: "100%",
+  height: null,
+  size: null, //폰트 크기
+  margin: "0px",
+  padding: "0px",
+  color: null, //폰트 색상
+  border: null,
+  radius: null, //border-radius
+  family: null, //폰트 타입
+  type: "text",
+  outline: false, // 아웃라인 유무
+  outlinecolor: null, // 아웃라인 색상
+  onSubmit: () => {},
+  value: false,
+};
 
 const InputText = styled.input`
-    ${(props) => (props.width ? `width: ${props.width};` : "")}
-    ${(props) => (props.height ? `height: ${props.height};` : "")}
+  ${(props) => (props.width ? `width: ${props.width};` : "")}
+  ${(props) => (props.height ? `height: ${props.height};` : "")}
     ${(props) => (props.size ? `font-size: ${props.size};` : "")}
     ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
     ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
@@ -72,16 +97,17 @@ const InputText = styled.input`
     ${(props) => (props.border ? `border: ${props.border};` : "")}
     ${(props) => (props.radius ? `border-radius: ${props.radius};` : "")}
     ${(props) => (props.family ? `font-family: ${props.family};` : "")}
-    ${(props) => (props.outlinecolor ? `outline-color: ${props.outlinecolor};` : "")}
+    ${(props) =>
+    props.outlinecolor ? `outline-color: ${props.outlinecolor};` : ""}
     ${(props) => (props.outline ? `outline: ${props.outline};` : "")}
     box-sizing: border-box;
 `;
 const InputTextArea = styled.textarea`
-    // 크기를 고정시킴.
-    resize: none; 
-    box-sizing: border-box;
-    ${(props) => (props.width ? `width: ${props.width};` : "")}
-    ${(props) => (props.height ? `height: ${props.height};` : "")}
+  // 크기를 고정시킴.
+  resize: none;
+  box-sizing: border-box;
+  ${(props) => (props.width ? `width: ${props.width};` : "")}
+  ${(props) => (props.height ? `height: ${props.height};` : "")}
     ${(props) => (props.size ? `font-size: ${props.size};` : "")}
     ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
     ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
@@ -89,7 +115,8 @@ const InputTextArea = styled.textarea`
     ${(props) => (props.border ? `border: ${props.border};` : "")}
     ${(props) => (props.radius ? `border-radius: ${props.radius};` : "")}
     ${(props) => (props.family ? `font-family: ${props.family};` : "")}
-    ${(props) => (props.outlinecolor ? `outline-color: ${props.outlinecolor};` : "")}
+    ${(props) =>
+    props.outlinecolor ? `outline-color: ${props.outlinecolor};` : ""}
     ${(props) => (props.outline ? `outline: ${props.outline};` : "")}
 `;
 
