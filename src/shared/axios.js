df045e9f793e51efc2febed6205ead40f7f35467
happyKale <syk767@gmail.com>
2021-10-13 20:31:15 +0900

    (김세연) 회원가입페이지에서 axios 테스트

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL : "http://localhost:4000/",
-    headers : {
-        'content-type' : 'application/json;charset=UTF-8',
-        Accept : 'application/json',
-    },
+  baseURL: "http://localhost:4000/",
+  headers: {
+    "content-type": "application/json;charset=UTF-8",
+    Accept: "application/json",
+  },
 });
 
-
-
 export const apis = {
-    getPost : () => instance.post('posts'),
-}
+  getPost: () => instance.post("posts"),
+  creatUser: (user) => instance.post("user", user),
+};
